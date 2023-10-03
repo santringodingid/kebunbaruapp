357b52361d6cea2599f268f5956ec3f21fb8518b
Santringoding <96803898+santringodingid@users.noreply.github.com>
2023-10-03 13:23:25 +0700

    PATH::Add Comeback and all method

--- application/controllers/Comeback.php
+++ application/controllers/Comeback.php
@@ -3,35 +3,104 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Comeback extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('baseModel');
-        $this->load->model('kamtibModel', 'km');
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('baseModel');
+		$this->load->model('KamtibModel', 'km');
+		$this->load->model('KembaliBanatModel', 'kem');
 
-        CekLoginAkses();
-    }
+		CekLoginAkses();
+	}
 
-    public function index()
-    {
-        $data = [
-            'title' => 'Cek Data Santri'
-        ];
+	public function index()
+	{
+		$data = [
+			'title' => 'Arus Balik Banat',
+			'kembali' => $this->kem->gettanggal(),
+			'domicile' => $this->kem->getDomicile()
+		];
 
-        $this->load->view('layout/header', $data);
-        $this->load->view('cekdata/cekdata');
-        $this->load->view('layout/footer');
-        $this->load->view('cekdata/javacekdata');
-    }
+		$this->load->view('layout/header', $data);
+		$this->load->view('kembalibanat/kembalibanat');
+		$this->load->view('layout/footer');
+		$this->load->view('kembalibanat/javakembalibanat');
+	}
 
-    public function loadData()
-    {
-        $loadData = $this->km->loadDataSantri();
+	public function tanggal()
+	{
+		$this->kem->tanggal();
 
-        $data = [
-            'datasantri' => $loadData
-        ];
+		redirect('kembalibanat');
+	}
 
-        $this->load->view('cekdata/ajax-cekdata', $data);
-    }
+	public function save()
+	{
+		$simpan = $this->kem->save();
+		$hasil = ['hasil' => $simpan];
+
+		echo json_encode($hasil);
+	}
+
+	public function getid()
+	{
+		$data = [
+			'hasil' => $this->kem->getID()
+		];
+		$this->load->view('kembalibanat/ajax-hasil', $data);
+	}
+
+	public function batal()
+	{
+		$this->kem->batal();
+	}
+
+	public function coba()
+	{
+		echo $this->kem->setDiff();
+	}
+
+	public function getmodal()
+	{
+		$data = [
+			'hasil' => $this->kem->getmodal()
+		];
+
+		$this->load->view('kembalibanat/ajax-modal', $data);
+	}
+
+	public function cekmodal()
+	{
+		$hasil = $this->kem->cekmodal();
+
+		echo json_encode($hasil);
+	}
+
+	public function saveijin()
+	{
+		$this->kem->saveijin();
+	}
+
+	public function getdata()
+	{
+		$data = [
+			'hasil' => $this->kem->getdata()
+		];
+
+		$this->load->view('kembalibanat/ajax-data', $data);
+	}
+
+	public function showfilter()
+	{
+		$data = [
+			'hasil' => $this->kem->showfilter()
+		];
+
+		$this->load->view('kembalibanat/ajax-filter', $data);
+	}
+
+	public function SetData()
+	{
+		$this->kem->setdata();
+	}
 }
