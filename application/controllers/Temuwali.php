f21434a0233f39d03aef210d5ed62730628a77cb
Santringoding <96803898+santringodingid@users.noreply.github.com>
2023-12-10 08:49:33 +0700

    ADD::temu wali -- cek data iv

--- application/controllers/Temuwali.php
+++ application/controllers/Temuwali.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-class TemuWali extends CI_Controller
+class Temuwali extends CI_Controller
 {
 	public function __construct()
 	{
